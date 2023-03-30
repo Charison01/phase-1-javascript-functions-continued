6b3ee3a12adf4d4f1b209c51adb49a9f4fd52034
Kelvin <kelvinkimaru@gmail.com>
2023-03-30 19:28:42 +0300

    Final Submission

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function saturdayFun(activity = "roller-skate"){
 return `This Saturday, I want to ${activity}!`
 }
 console.log(saturdayFun());
-
+//Defining a function using a function expression
 const mondayWork = function (work = "go to the office"){
     return `This Monday, I will ${work}.`
 }
@@ -12,6 +12,15 @@ console.log(mondayWork("going no where"));
 (function (){
     console.log("Yet more razzling");
 });
+//
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+console.log(wrapAdjective());
+  
+  
 //Anonymous fucntions passed as callback function
 //const button = document.getElementById("button");
 //button.addEventListener("click",function (){
